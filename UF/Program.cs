6d5575a52a4a138a0e2551d668795901a249db61
ebrahim <ebrahim3adel@gmail.com>
2024-06-02 20:25:24 +0300

    quick find implementation

---
+++ UF/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+var uf = new QuickFindUF(10);
+uf.Union(4, 3);
+uf.Union(3, 8);
+uf.Union(6, 5);
+uf.Union(9, 4);
+uf.Union(2, 1);
+Console.WriteLine(uf.Connected(8, 9)); // True
